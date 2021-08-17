f58d619a63c031b0a78f1d487041427da0d109de
brianmwangi <Nairobi@2020>
2021-08-17 09:05:39 +0300

    Changed to Gin and Cloudinary

--- main.go
+++ main.go
@@ -1,80 +1,146 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/cloudinary/cloudinary-go"
+	"github.com/cloudinary/cloudinary-go/api/admin"
+	"github.com/cloudinary/cloudinary-go/api/admin/search"
+	"github.com/cloudinary/cloudinary-go/api/uploader"
+	"github.com/gin-gonic/gin"
 )
 
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-	//  Ensure our file does not exceed 5MB
-	r.Body = http.MaxBytesReader(w, r.Body, 5*1024*1024)
+func uploadFile(c *gin.Context) {
 
-	file, handler, err := r.FormFile("file")
+	// Create our instance
+	cld, _ := cloudinary.NewFromURL("cloudinary://775819266531562:0vvHyxdwaqAHBjT44neW6h572HY@djmi67ke1")
 
-	// Capture any errors that may arise
+	// Get the preferred name of the file if its not supplied
+	fileName := c.PostForm("name")
+
+	// Add tags
+	fileTags := c.PostForm("tags")
+	file, _, err := c.Request.FormFile("file")
 	if err != nil {
-		fmt.Fprint(w, "Error getting the file")
-		fmt.Println(err)
-		return
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   err,
+			"message": "Failed to upload",
+		})
 	}
 
-	defer file.Close()
-
-	fmt.Printf("Uploaded file name: %+v\n", handler.Filename)
-	fmt.Printf("Uploaded file size %+v\n", handler.Size)
-	fmt.Printf("File mime type %+v\n", handler.Header)
-
-	// Get the file content type and access the file extension
-	fileType := strings.Split(handler.Header.Get("Content-Type"), "/")[1]
-
-	// Create the temporary file name
-	fileName := fmt.Sprintf("upload-*.%s", fileType)
-
-	// Create a temporary file with a dir folder
-	tempFile, err := ioutil.TempFile("temp-files", fileName)
+	result, err := cld.Upload.Upload(c, file, uploader.UploadParams{
+		PublicID: fileName,
+		// Split the tags by comma's
+		Tags: strings.Split(",", fileTags),
+	})
 
 	if err != nil {
-		fmt.Println(err)
+		c.String(http.StatusConflict, "Upload to cloudinary failed")
 	}
 
-	defer tempFile.Close()
-
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	tempFile.Write(fileBytes)
-	fmt.Fprintf(w, "Successfully uploaded file")
+	c.JSON(http.StatusCreated, gin.H{
+		"message":   "Successfully uploaded the file",
+		"secureURL": result.SecureURL,
+		"publicURL": result.URL,
+	})
 }
 
-func getUploadedFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("We are accessing files")
-	fileinfos, err := ioutil.ReadDir("temp-files")
+func getAllUploadedAssets(c *gin.Context) {
+	cld, _ := cloudinary.NewFromURL("cloudinary://775819266531562:0vvHyxdwaqAHBjT44neW6h572HY@djmi67ke1")
+	var urls []string
+
+	searchQ := search.Query{
+		Expression: "resource_type:image AND uploaded_at>1d AND bytes<1m",
+		SortBy:     []search.SortByField{{"created_at": search.Descending}},
+		MaxResults: 10,
+	}
+
+	results, err := cld.Admin.Search(c, searchQ)
 	if err != nil {
-		fmt.Println("Failed")
-		fmt.Println(err)
-
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":   err,
+			"message": "Failed to query your files",
+		})
 	}
 
-	for _, file := range fileinfos {
-		fmt.Println(file)
+	for _, asset := range results.Assets {
+		urls = append(urls, asset.SecureURL)
 	}
 
-	fmt.Fprintf(w, "Successfully got all files")
+	c.JSON(http.StatusAccepted, gin.H{
+		"success": true,
+		"data":    urls,
+	})
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "We are at home")
+func getUploadedFile(c *gin.Context) {
+	// Create our instance
+	cld, _ := cloudinary.NewFromURL("cloudinary://775819266531562:0vvHyxdwaqAHBjT44neW6h572HY@djmi67ke1")
+	fileName := c.Param("name")
+
+	// Access the filename using a desired filename
+	result, err := cld.Admin.Asset(c, admin.AssetParams{PublicID: fileName})
+	if err != nil {
+		c.String(http.StatusNotFound, "We were unable to find the file requested")
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{
+		"message":    "Successfully found the image",
+		"secureURL":  result.SecureURL,
+		"publicURL":  result.URL,
+		"created_at": result.CreatedAt.String(),
+	})
+}
+
+func updateFile(c *gin.Context) {
+	cld, _ := cloudinary.NewFromURL("cloudinary://775819266531562:0vvHyxdwaqAHBjT44neW6h572HY@djmi67ke1")
+	fileId := c.Param("publicId")
+	newFileName := c.PostForm("fileName")
+
+	// Access the filename using a desired filename
+	result, err := cld.Upload.Rename(c, uploader.RenameParams{
+		FromPublicID: fileId,
+		ToPublicID:   newFileName,
+	})
+	if err != nil {
+		c.String(http.StatusNotFound, "We were unable to find the file requested")
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{
+		"message":    "Successfully found the image",
+		"secureURL":  result.SecureURL,
+		"publicURL":  result.URL,
+		"created_at": result.CreatedAt.String(),
+	})
+}
+
+func deleteAsset(c *gin.Context) {
+	cld, _ := cloudinary.NewFromURL("cloudinary://775819266531562:0vvHyxdwaqAHBjT44neW6h572HY@djmi67ke1")
+	fileId := c.Param("assetId")
+	result, err := cld.Upload.Destroy(c, uploader.DestroyParams{PublicID: fileId})
+
+	if err != nil {
+		c.String(http.StatusBadRequest, "File could not be deleted")
+	}
+
+	c.JSON(http.StatusContinue, result.Result)
+}
+
+func home(c *gin.Context) {
+	c.String(200, "We are blessed")
 }
 
 func routes() {
-	http.HandleFunc("/upload", uploadFile)
-	http.HandleFunc("/getUploads", getUploadedFile)
-	http.HandleFunc("/", home)
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()
+	router.GET("/", home)
+	router.POST("/upload", uploadFile)
+	router.POST("/get-files", getAllUploadedAssets)
+	router.GET("/get-upload/:assetId", getUploadedFile)
+	router.PUT("/update-file/:name", updateFile)
+	router.DELETE("/delete-file/:assetId", deleteAsset)
+
+	router.Run(":1243")
 }
 
 func main() {
